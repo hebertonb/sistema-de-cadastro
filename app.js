517d14e808a4415a810da5513ac069a55f3ed84c
hebertonb <54633418+hebertonb@users.noreply.github.com>
2023-09-03 22:18:09 -0300

    nova edição do JS

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 
+console.log("Rodando o sistema de gerenciamento de jogos")
 
+console.log("Rodando o sistema de gerenciamento de jogos")
 
-
+console.log("Rodando o sistema de gerenciamento de jogos")
